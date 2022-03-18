4cf1b5f6bb66cb318f4d12d6c5bfca2877f60c08
Slava <slavapank94@yandex.ru>
2022-03-18 11:26:00 +0300

    desktop done

--- src/js/components/modalForm.js
+++ src/js/components/modalForm.js
@@ -1,20 +1,22 @@
 import { renderModalForm } from "../functions/renderModalForm";
 import vars from '../_vars';
 
-document.querySelector('.request__btn').addEventListener('click', () => {
-  vars.siteContainer.append(renderModalForm());
-  const scrollWrapper = document.querySelector('.simplebar-content-wrapper');
-  const scrollBar = document.querySelector('.simplebar-vertical');
-  const wrapper = document.querySelector('.wrapper');
-  const wrapperBg = document.querySelector('.wrapper__bg');
-  const wrapperSlide = document.querySelector('.wrapper__slide');
-  setTimeout(() => {
-    wrapperBg.classList.add('modal-is-open');
-    wrapperSlide.classList.add('wrapper-slide--current');
-  },300);
+document.querySelectorAll('.btn-open').forEach(item => {
+  item.addEventListener('click', () => {
+    vars.siteContainer.append(renderModalForm());
+    const scrollWrapper = document.querySelector('.simplebar-content-wrapper');
+    const scrollBar = document.querySelector('.simplebar-vertical');
+    const wrapper = document.querySelector('.wrapper');
+    const wrapperBg = document.querySelector('.wrapper__bg');
+    const wrapperSlide = document.querySelector('.wrapper__slide');
+    setTimeout(() => {
+      wrapperBg.classList.add('modal-is-open');
+      wrapperSlide.classList.add('wrapper-slide--current');
+    },300);
 
 
 
-  scrollWrapper.classList.add(vars.burgerClassesOverflow);
-  scrollBar.classList.add(vars.burgerClassesZIndex);
+    scrollWrapper.classList.add(vars.burgerClassesOverflow);
+    scrollBar.classList.add(vars.burgerClassesZIndex);
+  })
 })
