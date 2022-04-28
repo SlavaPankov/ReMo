0cb7e774c767adc56892403d7e407b730654e991
Slava <slavapank94@yandex.ru>
2022-04-28 12:31:48 +0300

    small-tablet

--- src/js/components/servicesSwiper.js
+++ src/js/components/servicesSwiper.js
@@ -10,20 +10,41 @@ import 'swiper/css/grid';
 Swiper.use([Pagination, Grid]);
 
 new Swiper('.services__swiper', {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
-  spaceBetween: 4,
-
-  modules: [Pagination, Grid],
-  grid: {
-    rows: 2,
-    fill: 'row'
-  },
-
   pagination: {
     clickable: true,
     el: '.services__pagination',
     bulletClass: 'maintenance__bullet',
     bulletActiveClass: 'maintenance__bullet-active',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      grid: {
+        rows: 1,
+        fill: 'row',
+      }
+    },
+
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 4,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      }
+    },
+
+    992 : {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 4,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+    }
   }
 });
